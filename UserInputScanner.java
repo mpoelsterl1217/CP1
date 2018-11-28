62941bc09e2fae78a6e3a397f21d597b0bfa6065
Matthew Poelsterl <mpoelsterl@I46221P64128304.local>
2018-11-28 14:52:08 -0600

    Finished User input scanner example

--- UserInputScanner.java
+++ UserInputScanner.java
@@ -2,12 +2,29 @@ import java.util.Scanner;
 
 public class UserInputScanner{
 	public static void main(String[] args){
+		//Create Scanner object
 		Scanner kin = new Scanner(System.in);
 
-		System.out.print("What is your name? ");
-		String name = kin.nextLine();
+		//Ask for name and say hello
+		System.out.print("What is your first name? ");
+		String firstName = kin.nextLine();
+		System.out.print("What is your last name? ");
+		String lastName = kin.nextLine();
+		System.out.println("Hello " + firstName + " " + lastName + ", it's nice to meet you!");
 
-		System.out.println("Hello " + name + ", it's nice to meet you!");
+		//Ask for Address, City, State, and Zip
+		System.out.print("What is street address? ");
+		String address = kin.nextLine();
+		System.out.print("What is your city? ");
+		String city = kin.nextLine();
+		System.out.print("What is your state? ");
+		String state = kin.nextLine();
+		System.out.print("What is your Zip code? ");
+		int zip = Integer.parseInt(kin.nextLine());
 
+		//Output Data about person
+		System.out.println("\n" + firstName + " " + lastName);
+		System.out.println(address);
+		System.out.println(city + ", " + state + " " + zip );
 	}
 }
