a237a0f6d9a83287c86b14c695a9781f47350f89
Matthew Poelsterl <mpoelsterl@I46221P64128304.local>
2018-12-17 14:49:41 -0600

    Scaffolding

--- CashRegister.java
+++ CashRegister.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class CashRegister {
   public static void main(String[] args) {
-  
+    //Create Scanner object
+    Scanner scan = new Scanner(System.in);
+
+    //Read Bill amount and payment
+    Double bill = scan.next();
+    Double payment = scan.next();
+
+    //Calculate Change Total and running remainder
+    Double ChangeTotal = payment - bill;
+    Double remainder = ChangeTotal;
+
+    //Calculate how many twenties
+
+    //Calculate how many tens
+
+    //Calculate fives
+
+    //Calculate singles
+
+    //Calculate quarters
+
+    //Calculate dimes
+
+    //Calculate nickels
+
+    //Calculate pennies
+
+    //Output Change due and breakdown of change
   }
 }
