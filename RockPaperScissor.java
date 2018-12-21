c424b1f60f205111712459047636c1e027ac50f8
Matthew Poelsterl <mpoelsterl@I46221P64128304.local>
2018-12-21 15:11:38 -0600

    passing input into determineWinner not working but strings are

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -5,15 +5,26 @@ public class RockPaperScissor {
     //Create a scanner
     Scanner scan = new Scanner(System.in);
 
-    //Get player1
+    //Get player1 input
+    System.out.println("Player 1:");
+    String move1 = scan.next();
+    System.out.println(move1);
 
     //Type stuff to hide other player move
+    for (int i = 0; i <20; i++) {
+      System.out.println("-------------------");
+    }
 
     //Get player2 input
+    System.out.println("Player 2:");
+    String move2 = scan.next();
+    System.out.println(move2);
 
     //output winner
-    System.out.println(determineWinner("rock", "scissors"));
+    System.out.println(determineWinner(move1, move2));
+    System.out.println(determineWinner("rock","paper"));
   }
+
   public static String determineWinner(String player1, String player2) {
     //Determine and return winner
     if ((player1 == "rock" && player2 == "scissors") || (player1 == "paper" && player2 == "rock") || (player1 == "scissors" && player2 == "paper")) {
