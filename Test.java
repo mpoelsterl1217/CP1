cbbe0e671d6d77f17d9b518cb6bf6da0cced8f84
Matthew Poelsterl <mpoelsterl@I46221P64128304.local>
2019-01-09 14:38:00 -0600

    substring worksheet

--- Test.java
+++ Test.java
@@ -1,6 +1,35 @@
 public class Test{
   public static void main(String[] args) {
-    System.out.println(11 / 5 + 11 % 5);
-    System.out.println(4.3 * 2 + 8 / 3 - 5);
+    System.out.println("Samurai".substring(0,3));
+
+    String str = "How much wood could a woodchuck chuck?";
+    String sub = str.substring(32);
+    String substr = str.substring(0,4);
+    System.out.println(sub + substr);
+
+    String s1 = "Mr. Berg";
+    String s2 = "Mrs. Roscoe";
+    String s3 = "Mr. Patel";
+    String s1sub = s1.substring(4);
+    String s2sub = s2.substring(5);
+    String s3sub = s3.substring(4);
+    System.out.println(s1sub + s2sub + s3sub);
+
+
+    String fName = "Robert";
+    String lName = "Berg";
+
+    String fLet1 = fName.substring(0,1);
+    String fLet2 = lName.substring(0,1);
+
+    String rest1 = fName.substring(1);
+    String rest2 = lName.substring(1);
+
+    String pl1 = rest1 + fLet1 + "ay";
+    String pl2 = rest2 + fLet2 + "ay";
+
+    System.out.println(pl1);
+    System.out.println(pl2);
+
   }
 }
