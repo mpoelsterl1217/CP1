d323eff1295d284732e7c4c24bf9f71a8d075b02
Matthew Poelsterl <mpoelsterl@I46221P64128304.local>
2019-01-11 15:11:41 -0600

    start of tic tac toe app

---
+++ TicTacToe.java
@@ -0,0 +1,13 @@
+public class TicTacToe {
+  public static void main(String[] args) {
+    String[] positions = new String[9];
+
+  }
+  public static boolean checkForWinnningState() {
+    //checks board state and returns a boolean
+    //Check
+  }
+  public static void runTurn() {
+    //outputs board and and takes in turn and updates board position
+  }
+}
