68330de06197eac6f99b416b8b20d7803cd94a49
Matthew Poelsterl <mpoelsterl@I46221P64128304.local>
2019-01-16 14:58:49 -0600

    Went over Frame Demo and added comments

--- JavaFrameDemo/FrameDemo.java
+++ JavaFrameDemo/FrameDemo.java
@@ -3,11 +3,11 @@ import javax.swing.JFrame;
 public class FrameDemo{
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Frame Demo");  //creates the frame object
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //sets what happens when you click X
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //sets what happens when you click X, closes operation
 		frame.setSize(999, 999); //sets the size of the frame
 
-    Shapes r = new Shapes();
-	  frame.add(r);
+    Shapes r = new Shapes(); //new shapes object named r
+	  frame.add(r); //puts the shapes object r in the frame
 
 		frame.setVisible(true); //makes the frame visible
 	}
--- JavaFrameDemo/Shapes.java
+++ JavaFrameDemo/Shapes.java
@@ -1,22 +1,24 @@
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
+import java.awt.*;
 import java.awt.geom.Ellipse2D;
-import java.awt.Color;
+
 import javax.swing.JComponent;
 
 public class Shapes extends JComponent{
 
 	public void paintComponent(Graphics g){
-		Graphics2D g2 = (Graphics2D) g;
+		Graphics2D g2 = (Graphics2D) g; //creates 2d graphics object
 
-		Rectangle box = new Rectangle(5, 10, 50, 30);
-		g2.draw(box);
+		Rectangle box = new Rectangle(5, 10, 50, 30); //creates Rectangle object at 5,10 of width 50, height 30
+		g2.draw(box); //Graphics object draws Rectangle box onto the canvas
 
-		g2.setColor(Color.blue);
+		g2.setColor(Color.blue); //Sets pen color to blue
 
-		Ellipse2D.Double circle = new Ellipse2D.Double(100, 100, 200, 200);
-		g2.fill(circle);
+		Ellipse2D.Double circle = new Ellipse2D.Double(100, 100, 200, 200); //Creates Ellipes object at 100,100 with width 200, height 200
+		g2.fill(circle); //fills the circle with blue
 
+		g2.setColor(Color.black);
+		Rectangle bb = new Rectangle(100,100,200,200);
+		g2.draw(bb);
+		
 	}
 }
