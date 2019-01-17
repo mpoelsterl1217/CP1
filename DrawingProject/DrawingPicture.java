bdfe869e5d014e9cf49112e4fe35eb6587669c06
Matthew Poelsterl <mpoelsterl@I46221P64128304.local>
2019-01-17 14:28:05 -0600

    Import DrawingPicture project

---
+++ DrawingProject/DrawingPicture.java
@@ -0,0 +1,42 @@
+/**
+ * Write a description of class LoopDrawing here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+public class DrawingPicture extends JComponent
+{
+    public void paintComponent(Graphics g){
+
+        Graphics2D g2 = (Graphics2D)g;
+
+	/******** Put your code here  *********/
+
+
+
+
+
+   	// comment this out once you have finished
+        drawGrid(g2);
+    }
+    public void drawGrid(Graphics g2){
+        g2.setColor(new Color(211, 211, 211));
+
+        // Draw vertical lines
+        for(int x = 0; x < 1000; x += 50){
+            g2.drawLine(x, 0, x, 1000);
+            g2.drawString("" + x, x, 10);
+        }
+
+        // Draw horizontal lines
+        for(int y = 50; y < 1000; y += 50){
+            g2.drawLine(0, y, 1000, y);
+            g2.drawString("" + y, 0, y);
+        }
+    }
+}
