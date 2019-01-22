2717ec95e8ac98477bab06a562d984864c9a5d63
Matthew Poelsterl <mpoelsterl@I46221P64128304.local>
2019-01-22 15:18:52 -0600

    star ack

--- DrawingProject/DrawingPicture.java
+++ DrawingProject/DrawingPicture.java
@@ -20,7 +20,7 @@ public class DrawingPicture extends JComponent
         BasicStroke gridStroke = new BasicStroke(1.0f);
         g2.setStroke(thick);
         Ellipse2D.Double logoRing = new Ellipse2D.Double(120, 120, 280, 280); //Creates Ellipes object at 100,100 with width 200, height 200
-        g2.draw(logoRing); //fills the circle with blue
+        //g2.draw(logoRing); //fills the circle with blue
 
         g2.setStroke(gridStroke);
         Ellipse2D.Double guideCircleCenter = new Ellipse2D.Double(160, 160, 200, 200); //Creates Ellipes object at 100,100 with width 200, height 200
@@ -38,7 +38,13 @@ public class DrawingPicture extends JComponent
         Ellipse2D.Double guideCircle6 = new Ellipse2D.Double(247, 210, 200, 200); //Creates Ellipes object at 100,100 with width 200, height 200
         g2.draw(guideCircle6); //fills the circle with blue
 
-        
+        int[] xPoints = new int[] {260, 277, 347, 293, 347, 277, 260, 243, 173, 227, 173, 243};
+        int[] yPoints = new int[] {160, 227, 210, 260, 310, 293, 360, 293, 310, 260, 210, 227};
+        Polygon star = new Polygon(xPoints, yPoints, 12);
+        g2.drawPolygon(star);
+
+        Polygon hex = new Polygon(yPoints, xPoints, 6);
+        g2.drawPolygon(hex);
         // comment this out once you have finished
         g2.setStroke(gridStroke);
         drawGrid(g2);
