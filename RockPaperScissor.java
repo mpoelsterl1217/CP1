82a863d95eaa0efbf0f2b1ae4b77a1852332f0e7
pastramafoo <mpoelsterl1217@gmail.com>
2019-05-23 19:33:17 -0500

    Commented array game, fixed rock paper scissors

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -6,8 +6,8 @@ public class RockPaperScissor {
     Scanner scan = new Scanner(System.in);
 
     //Get player1 input
-    System.out.print("Player 1:");
-    string move1 = scan.next();
+    System.out.print("Player 1:  ");
+    String move1 = scan.next();
 
     //Type stuff to hide other player move
     for (int i = 0; i <20; i++) {
@@ -15,22 +15,19 @@ public class RockPaperScissor {
     }
 
     //Get player2 input
-    System.out.print("Player 2:");
-    string move2 = scan.next();
+    System.out.print("Player 2: ");
+    String move2 = scan.next();
 
     //output winner
-    System.out.println(move1);
-    System.out.println(move2);
-    System.out.println(determineWinner(move1, move2));
-    System.out.println(determineWinner("rock","paper"));
+    System.out.println("\n" + determineWinner(move1, move2));
   }
 
   public static String determineWinner(String player1, String player2) {
     //Determine and return winner
-    if ((player1 == "rock" && player2 == "scissors") || (player1 == "paper" && player2 == "rock") || (player1 == "scissors" && player2 == "paper")) {
+    if ((player1.equals("rock") && player2.equals("scissors")) || (player1.equals("paper") && player2.equals("rock")) || (player1.equals("scissors") && player2.equals("paper"))) {
       //player1 wins
       return "Player 1 wins";
-    } else if ((player2 == "rock" && player1 == "scissors") || (player2 == "paper" && player1 == "rock") || (player2 == "scissors" && player1 == "paper")) {
+    } else if ((player2.equals("rock") && player1.equals("scissors")) || (player2.equals("paper") && player1.equals("rock")) || (player2.equals("scissors") && player1.equals("paper"))) {
       //player2 wins
       return "Player 2 wins";
     } else {
